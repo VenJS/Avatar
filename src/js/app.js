229a9de0b74b490515c6caa65825f2a24d0c4618
Venci <venci34@gmail.com>
2021-12-30 17:18:14 +0200

    class active added

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 import "../scss/app.scss";
 
+
+
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
@@ -8,3 +10,6 @@ window.addEventListener("DOMContentLoaded", () => {
     alert("💣");
   });
 });
+
+
+
